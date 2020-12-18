d5b29b686b47c66026b4d6ecbbff5a44489dfb90
240648795 <240648795@qq.com>
2020-12-18 11:32:54 +0800

    预测宏观因子的程序进行修改

--- wa_coor_analysis.py
+++ wa_coor_analysis.py
@@ -19,6 +19,7 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 mfont = FontProperties(fname=r"C:\Windows\Fonts\simhei.ttf", size=14)
 
 
+# 插值
 def create_interpolate_feature(filename, outputfilename):
     data = pd.read_csv(filename)
     date = data['日期']
@@ -30,6 +31,7 @@ def create_interpolate_feature(filename, outputfilename):
     pass
 
 
+# 预测一个字段的arima数据
 def get_sarimax_predict(data, start_time, end_time, order, seasonal_order):
     data.columns = ['rtime', 'values']
     data['rtime'] = data['rtime'].apply(lambda x: datetime.strptime(x, '%Y/%m'))
@@ -51,7 +53,8 @@ def get_sarimax_predict(data, start_time, end_time, order, seasonal_order):
     return y_pred
 
 
-def create_arima_feature(filename, outputfilename):
+# 预测所有字段的arima数据，参数(1, 1, 1), (1, 1, 1, 12)
+def create_arima_feature(filename, start_time, end_time, outputfilename):
     data = pd.read_csv(filename)
     date = data['日期']
     data_to_cal_col = []
@@ -64,17 +67,22 @@ def create_arima_feature(filename, outputfilename):
     y_pred_total = []
     for i, val in enumerate(columns):
         tempi = data[['日期', val]]
-        y_predi = get_sarimax_predict(tempi, '2020-11-01', '2021-12-01', (1, 1, 1), (1, 1, 1, 12))
+        y_predi = get_sarimax_predict(tempi, start_time, end_time, (1, 1, 1), (1, 1, 1, 12))
+        # print (y_predi)
         y_predi = np.asarray(y_predi)
         y_pred_total.append(y_predi)
 
     y_pred_total = np.asarray(y_pred_total)
     y_pred_total_np = y_pred_total.T
     y_pred_total_df = pd.DataFrame(y_pred_total_np)
-    print(y_pred_total_df)
+    y_pred_total_df.columns = columns
+    y_pred_date = pd.date_range(start_time, end_time, freq='MS')
+    y_pred_total_df['日期'] = y_pred_date
+    y_pred_total_df.to_csv(outputfilename)
     pass
 
 
+# 按照差分生成宏观因子数据
 def create_m_feature(filename, outputfilename, skip_rows):
     data = pd.read_csv(filename)
     date = data['日期']
@@ -113,7 +121,8 @@ def create_m_feature(filename, outputfilename, skip_rows):
     pass
 
 
-def create_macro_wa_coor_fig(macro_filename, wa_index_name, oufputpath,type='m'):
+# 做一个相关性图表（挖）
+def create_macro_wa_coor_fig(macro_filename, wa_index_name, oufputpath, type='m'):
     macro_index = pd.read_excel(macro_filename, skiprows=5, header=None)
     macro_index = macro_index[:macro_index.shape[0] - 2]
     macro_index_name_strings = macro_filename.split('/')
@@ -121,7 +130,7 @@ def create_macro_wa_coor_fig(macro_filename, wa_index_name, oufputpath,type='m')
     macro_index_name = macro_index_name_xls[:len(macro_index_name_xls) - 4]
 
     macro_index.columns = ['日期', macro_index_name]
-    if type=='y':
+    if type == 'y':
         macro_index['日期'] = macro_index['日期'].apply(lambda x: str(x.year))
         macro_index['日期'] = macro_index['日期'].apply(lambda x: datetime.strptime(x, '%Y'))
     else:
@@ -163,7 +172,7 @@ def create_macro_wa_coor_fig(macro_filename, wa_index_name, oufputpath,type='m')
     total_index = min_max_scaler_model.fit_transform(total_xi_df)
 
     y1 = total_index[:, 0]
-    for i in range(1,len(total_xi_df.columns)):
+    for i in range(1, len(total_xi_df.columns)):
         y2 = total_index[:, i]
         coor_num = round(
             np.corrcoef(y1, y2)[0, 1], 2)
@@ -194,55 +203,11 @@ def create_macro_wa_coor_fig(macro_filename, wa_index_name, oufputpath,type='m')
             plt.savefig(oufputpath + '/other/' +
                         total_xi_df.columns[0] + '与' + total_xi_df.columns[i] + '，相关系数为：' + str(coor_num) + '.png',
                         bbox_inches='tight')
-
-    # y1 = total_index[:, 0]
-    # y2 = total_index[:, 1]
-    # coor_num = round(
-    #     np.corrcoef(y1, y2)[0, 1], 2)
-    # plt.clf()
-    # plt.title(total_xi_df.columns[0] + '与' + total_xi_df.columns[1] + '的相关分析(' + str(coor_num) + ')',
-    #           fontproperties=mfont)
-    # plt.xlabel('日期')
-    # plt.ylabel('值')
-    # # xs = [datetime.strptime(d, '%Y/%m').date() for d in date]
-    # p1 = plt.plot(date, y1)
-    # p2 = plt.plot(date, y2)
-    # plt.gcf().autofmt_xdate()
-    # plt.legend([p1, p2], labels=[total_xi_df.columns[0], total_xi_df.columns[1]])
-    #
-    # if abs(coor_num) >= 0.7 and abs(coor_num) < 1:
-    #     plt.savefig(oufputpath + '/70/' +
-    #                 total_xi_df.columns[0] + '与' + total_xi_df.columns[1] + '，相关系数为：' + str(coor_num) + '.png',
-    #                 bbox_inches='tight')
-    # elif abs(coor_num) >= 0.6 and abs(coor_num) < 0.7:
-    #     plt.savefig(oufputpath + '/60/' +
-    #                 total_xi_df.columns[0] + '与' + total_xi_df.columns[1] + '，相关系数为：' + str(coor_num) + '.png',
-    #                 bbox_inches='tight')
-    # elif abs(coor_num) >= 0.5 and abs(coor_num) < 0.6:
-    #     plt.savefig(oufputpath + '/50/' +
-    #                 total_xi_df.columns[0] + '与' + total_xi_df.columns[1] + '，相关系数为：' + str(coor_num) + '.png',
-    #                 bbox_inches='tight')
-    # else:
-    #     plt.savefig(oufputpath + '/other/' +
-    #                 total_xi_df.columns[0] + '与' + total_xi_df.columns[1] + '，相关系数为：' + str(coor_num) + '.png',
-    #                 bbox_inches='tight')
     pass
 
 
-if __name__ == '__main__':
-    # 预测宏观因子
-    # create_arima_feature(r'./data/wa_index/features_month_interpolate.csv',
-    #                      r'./data/wa_index/features_month_interpolate_m1-2.csv')
-
-    # 生成m1到m2的宏观因子数据
-    # create_m_feature(r'./data/wa_index/features_month_interpolate.csv',
-    #                  r'./data/wa_index/features_month_interpolate_m1-2.csv', skip_rows=2)
-
-    # 生成一个相关性图表
-    # create_macro_wa_coor_fig(r'./data/wa_index/wa_macro_index/month/PPI按大类分(月)（累计同比）.xls',
-    #                          r'./data/wa_index/wa_month.csv',
-    #                          r'./data/wa_index/wa_png/month')
-
+# 做所有相关性图表（挖）
+def create_macro_wa_coor_fig_main():
     # 按月生成图表
     file_dir = r'./data/wa_index/wa_macro_index/month/'
     wa_index_filename = r'./data/wa_index/wa_month.csv'
@@ -252,7 +217,7 @@ if __name__ == '__main__':
             one_macro_index = root + file
             create_macro_wa_coor_fig(one_macro_index,
                                      wa_index_filename,
-                                     output_filepath,type='m')
+                                     output_filepath, type='m')
 
     # 按季生成图表
     file_dir = r'./data/wa_index/wa_macro_index/quarter/'
@@ -263,7 +228,7 @@ if __name__ == '__main__':
             one_macro_index = root + file
             create_macro_wa_coor_fig(one_macro_index,
                                      wa_index_filename,
-                                     output_filepath,type='m')
+                                     output_filepath, type='m')
 
     # 按年生成图表
     file_dir = r'./data/wa_index/wa_macro_index/year/'
@@ -273,12 +238,22 @@ if __name__ == '__main__':
         for file in files:
             one_macro_index = root + file
             create_macro_wa_coor_fig(one_macro_index,
-                                    wa_index_filename,
-                                    output_filepath,type='y')
+                                     wa_index_filename,
+                                     output_filepath, type='y')
 
-
+    # 生成一个相关性图表
     # create_macro_wa_coor_fig(r'./data/wa_index/wa_macro_index/month/PPI按大类分(月).xls',
     #                          r'./data/wa_index/wa_month.csv',
     #                          r'./data/wa_index/wa_png/month',type='m')
+
+
+if __name__ == '__main__':
+    # 预测宏观因子
+    create_arima_feature(r'./data/wa_index/features_month_interpolate.csv','2020-11-01', '2021-11-01',
+                         r'./data/wa_index/features_month_interpolate_arima_predict.csv')
+
+    # 生成m1到m2的宏观因子数据
+    # create_m_feature(r'./data/wa_index/features_month_interpolate.csv',
+    #                  r'./data/wa_index/features_month_interpolate_m1-2.csv', skip_rows=2)
 
     pass
